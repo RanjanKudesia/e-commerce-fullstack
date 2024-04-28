1b043b898f04d183ad9dca5f563303eef1699f68
Shanya-Sahu <shanyasahu11@gmail.com>
2024-04-28 20:14:58 +0530

    Global state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GlobalStateProvider>
         <Navbar/>
         {children}
         <Footer/>
+        </GlobalStateProvider>
         </body>
     </html>
   );
