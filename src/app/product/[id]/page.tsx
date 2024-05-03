6dd28ea62aa0d44888e2e3842bc34ebce166acc4
Shanya-Sahu <shanyasahu11@gmail.com>
2024-05-03 12:27:10 +0530

    update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,15 +1,19 @@
 import MainImage from "./components/main-image";
 import { Suspense } from "react";
-
+import FirstSec from "./components/first";
+import SecondSec from "./components/second";
+import ThirdSec from "./components/third";
 
 
 
 export default function Product() {
     return (
-        <Suspense fallback={<h1 className="w-full text-center">Loading...</h1>}div className="xl:px-40 md:py-12 px-5 py-5">
-            <FirstSec/>
-        <SecondSec/>
-        <ThirdSec/>
-        </Suspensediv>
+        <Suspense fallback={<h1 className="w-full text-center">Loading...</h1>}>
+            <div className="xl:px-40 md:py-12 px-5 py-5">
+                <FirstSec/>
+                <SecondSec/>
+                <ThirdSec/>
+            </div>
+        </Suspense>
     )
 }
